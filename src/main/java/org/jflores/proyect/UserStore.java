9e56df2c235e82e32fec639f3961e66ac2d8db45
jose-1991 <jose.floresc.java@gmail.com>
2021-12-01 11:08:09 -0400

    extraer el metodo leerArchivoCSV del main

--- src/main/java/org/jflores/proyect/UserStore.java
+++ src/main/java/org/jflores/proyect/UserStore.java
@@ -3,44 +3,47 @@ package org.jflores.proyect;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class UserStore {
-    public static final String SEPARADOR = ",";
-
     public static void main(String[] args) {
+        System.out.println(leerArchivoCSV("C:\\Users\\JoSe\\Desktop\\Proyecto1\\datosbd.csv"));
+    }
 
+    public static List<String> leerArchivoCSV(String file) {
+
+        final String SEPARADOR = ",";
         BufferedReader bufferLectura = null;
+        List<String> linea = new ArrayList<>();
         try {
-
-            if (!file.endsWith(".csv")){
+            if (!file.endsWith(".csv")) {
                 throw new Exception();
             }
-
             bufferLectura = new BufferedReader(new FileReader(file));
-
-                   String line = bufferLectura.readLine();
-
+            String line = bufferLectura.readLine();
 
             while (line != null) {
 
                 String[] campos = line.split(SEPARADOR);
-                linea.add(Arrays.toString(campos)+"\n");
+                linea.add(campos + "\n");
                 line = bufferLectura.readLine();
 
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
+            }
+        } catch (IOException e) {
+            System.out.println("Archivo no encontrado!");
+        } catch (Exception ex) {
+            System.out.println("seleccione un archivo con extension .csv");
+        } finally {
             if (bufferLectura != null) {
                 try {
                     bufferLectura.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.out.println(" ocurrio un error al leer el archivo");
                 }
             }
         }
+        return linea;
     }
 }
